d27a6a8f556ce2546bd6410aa9300a30cd9acf6a
aryan-np <nparyan7@gmail.com>
2025-06-17 09:22:03 +0545

    minor fixes

--- Todo/Todo/models.py
+++ Todo/Todo/models.py
@@ -19,4 +19,6 @@ class TodoModel(models.Model):
     status = models.IntegerField(choices=Status.choices, default= Status.PENDING)
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.title
     

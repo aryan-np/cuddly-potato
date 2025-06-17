5263476aa85dec616c448131a93c5f2fc5dab6a3
aryan-np <nparyan7@gmail.com>
2025-06-17 12:35:23 +0545

    add password reset urls

--- Todo/Todo/urls.py
+++ Todo/Todo/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path,include
 
 from . import views
@@ -16,5 +17,11 @@ urlpatterns = [
     path('api/register/', views.register,name='register'),
     path('api/todo/search/', views.search,name='search'),
     path('api/token/refresh',TokenRefreshView.as_view(),name = 'token refresh'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    
+    # Password reset urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
